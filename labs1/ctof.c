dc41b42a86b86090993f94df9c56dbdcd964a18d
jingzijin <2226920290@qq.com>
2019-10-28 00:04:50 -0700

    added ctof.c file

--- labs1/ctof.c
+++ labs1/ctof.c
@@ -1,1 +1,19 @@
-#include <studio.h>
+#include <stdio.h>
+
+main()
+{
+    int celsius , fahr;
+    int lower , upper , step;
+    printf("celsius, fahr\n");
+
+    lower = 0;
+    upper = 100;
+    step = 5;
+
+    celsius = lower;
+    while (celsius <= upper) {
+        fahr = celsius * 9 / 5 + 32;
+        printf("%d\t%d\n", celsius, fahr );
+        celsius = celsius + step;
+    }
+}
