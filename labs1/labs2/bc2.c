b534cc5dba18b54a355dd0a3f1bf9be7a9edce6c
jingzijin <2226920290@qq.com>
2019-10-31 07:12:53 -0700

    added to bc2.c file

--- labs1/labs2/bc2.c
+++ labs1/labs2/bc2.c
@@ -2,15 +2,15 @@
 main()
 {
     int count = 1;
-    int number = 0;
-    int anothernumber = 0;
-    printf("please input the number\n");
-    scanf("%5d", &anothernumber);
-    while(anothernumber  !=1)
+    int x = 0;
+    int y = 0;
+    printf("please input the x\n");
+    scanf("%5d", &y);
+    while(y !=1)
     {
-        number = anothernumber%2;
-        anothernumber = anothernumber/2;
-        if(number  ==1)
+        x = y%2;
+        y = y/2;
+        if(x  ==1)
         count++;
     }
     printf("the amount is %d\n",count);
