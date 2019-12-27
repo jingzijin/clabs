7b3610953f6a8640c7fa0ef3ff731123548238bc
jingzijin <2226920290@qq.com>
2019-12-26 23:31:41 -0800

    added to ex2.c file

--- labs3/ex2.c
+++ labs3/ex2.c
@@ -17,21 +17,28 @@ int main()
         exit(0);
     }
     c = getchar();
-    while (c !='\n'&&c !=EOF){
+    while (c !='\n'){
         if((c>='0'&&c<='9')||(c>='a'&&c <= 'f')||(c>='A'&&c <= 'F')){
             if(c=='a'||c=='A'){
                 x = x * 16 + 10;
-            }else if(c=='b'||c=='B'){
+            }
+            else if(c=='b'||c=='B'){
                 x = x * 16 + 11;
-            }else if(c=='c'||c=='C'){
+            }
+            else if(c=='c'||c=='C'){
                 x = x * 16 + 12;
-            }else if(c=='d'||c=='D'){
+            }
+            else if(c=='d'||c=='D'){
                 x = x * 16 + 13;
-            }else if(c=='e'||c=='E'){
+            }
+            else if(c=='e'||c=='E'){
                 x = x * 16 + 14;
-            }else if(c=='f'||c=='F'){
+            }
+            else if(c=='f'||c=='F'){
                 x = x * 16 + 15;
-            }else{x = x * 16 + (c - '0');
+            }
+            else{
+                x = x * 16 + (c - '0');
             }
             c = getchar();
         }else{
@@ -43,11 +50,12 @@ int main()
     unsigned int j = 0;
     unsigned int k = 0-1;
     while (i != 0){
-    i = i <<1;
+    i = i <<4;
     j = j + 1;
     }
-    if(x > k){
-        printf("输入的数字太大，需要换一个小的数字,最大不超过k\n");
+    j = j + 1;
+    if(x > j){
+        printf("输入的数字太大，需要换一个小的数字,最大不超过%u\n",k);
         exit(0);
     }else{
         printf("%d\n",x);
